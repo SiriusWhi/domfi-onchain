5cafd3481af9114258e335a57ec34f318df1ba0c
SiriusWhi <dprripontipo480@gmail.com>
2021-07-26 17:29:35 -0500

    Fix investor allocation amounts
    BN can't handle scientific notation but BigNumber does

--- migrations/2_token.js
+++ migrations/2_token.js
@@ -10,5 +10,5 @@ module.exports = async function (deployer, network, accounts) {
     // In a test environment an ERC777 token requires deploying an ERC1820 registry
     await singletons.ERC1820Registry(accounts[0]);
   }
-  await deployer.deploy(DomToken, [accounts[0], '0x0000000000000000000000000000000000000001']); // DAO address goes here
+  await deployer.deploy(DomToken, [accounts[0], '0xFd3475241a5759E87c22f14B30f01622d4B5a49C']); // DAO address goes here
 };
--- migrations/4_investor_allocations.js
+++ migrations/4_investor_allocations.js
@@ -1,28 +1,30 @@
+const BigNumber = require('bignumber.js');
+
 const DomToken = artifacts.require("DominationToken");
 const VesterFactory = artifacts.require("VesterFactory");
 
 const allocations = [
-    { address: '0x0000000000000000000000000000000000000001', amount: web3.utils.toBN("700000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount: web3.utils.toBN("500000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount: web3.utils.toBN("500000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount: web3.utils.toBN("300000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount: web3.utils.toBN("200000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount: web3.utils.toBN("150000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount: web3.utils.toBN("150000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount:  web3.utils.toBN("75000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount:  web3.utils.toBN("60000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount:  web3.utils.toBN("50000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount:  web3.utils.toBN("50000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount:  web3.utils.toBN("25000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount:  web3.utils.toBN("20000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount:  web3.utils.toBN("20000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount:  web3.utils.toBN("20000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount:  web3.utils.toBN("10000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount:  web3.utils.toBN("10000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount: web3.utils.toBN("790000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount: web3.utils.toBN("790000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount: web3.utils.toBN("790000e20") },
-    { address: '0x0000000000000000000000000000000000000001', amount: web3.utils.toBN("790000e20") },
+    { address: '0x0000000000000000000000000000000000000001', amount:  new BigNumber("70e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:  new BigNumber("50e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:  new BigNumber("50e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:  new BigNumber("30e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:  new BigNumber("20e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:  new BigNumber("15e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:  new BigNumber("15e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount: new BigNumber("7.5e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:   new BigNumber("6e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:   new BigNumber("5e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:   new BigNumber("5e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount: new BigNumber("2.5e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:   new BigNumber("2e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:   new BigNumber("2e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:   new BigNumber("2e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:   new BigNumber("1e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:   new BigNumber("1e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:  new BigNumber("79e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:  new BigNumber("79e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:  new BigNumber("79e24") },
+    { address: '0x0000000000000000000000000000000000000001', amount:  new BigNumber("79e24") },
 ]
 
 module.exports = async function (deployer, network, accounts) {
@@ -36,12 +38,14 @@ module.exports = async function (deployer, network, accounts) {
             DomToken.address,
             allocation.address,
             allocation.amount,
-            1626307200, // vestingBegin, 2021-07-15
-            1642204800, // vestingCliff, 2022-01-15. 6 months
-            1721001600, // vestingEnd, 2024-07-15. 3 years
+            1629032400, // vestingBegin, 2021-08-15
+            1644930000, // vestingCliff, 2022-02-15. 6 months
+            1721048400, // vestingEnd, 2024-08-15. 3 years
             2592000     // timeout, 1 month
         );
         const vester = result.logs[0].args[0]; // hack, should wait for an event to be emitted
+
+        console.log(vester);
 
         // fund the contract
         await dom.send(vester, allocation.amount, 0);
