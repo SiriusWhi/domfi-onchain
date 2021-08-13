d3faea4ae4247d0497e8fea7d0db3e80c61b4677
SiriusWhi <dprripontipo480@gmail.com>
2021-08-13 14:32:32 -0500

    Remove unused interface and error strings

--- contracts/staking/utils/Errors.sol
+++ contracts/staking/utils/Errors.sol
@@ -7,10 +7,7 @@ abstract contract Errors {
     string internal constant ERROR_NOT_ENOUGH_ALLOWANCE = "ERROR_NOT_ENOUGH_ALLOWANCE";
     string internal constant ERROR_NOT_ENOUGH_STAKE = "ERROR_NOT_ENOUGH_STAKE";
     string internal constant ERROR_NOT_A_CONTRACT = "ERROR_NOT_A_CONTRACT";
-    string internal constant ERROR_ONLY_OWNER = "ERROR_ONLY_OWNER";
-    string internal constant ERROR_REENTRANCY = "ERROR_REENTRANCY";
     string internal constant ERROR_STAKING_NOT_STARTED = "ERROR_STAKING_NOT_STARTED";
     string internal constant ERROR_STAKING_ENDED_OR_NOT_STARTED = "ERROR_STAKING_ENDED_OR_NOT_STARTED";
-    string internal constant ERROR_ZERO_ADDRESS = "ERROR_ZERO_ADDRESS";
     string internal constant ERROR_ZERO_AMOUNT = "ERROR_ZERO_AMOUNT";
 }
