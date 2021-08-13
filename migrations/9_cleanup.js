132dd603f9f4ec11f9a607e12c19c6b0f11f93de
SiriusWhi <dprripontipo480@gmail.com>
2021-08-13 12:28:23 -0500

    Remove admin role from deployer after deployment

--- migrations/9_cleanup.js
+++ migrations/9_cleanup.js
@@ -1,7 +1,10 @@
 const DomToken = artifacts.require("DominationToken");
 
+const DEFAULT_ADMIN_ROLE = '0x00';
+
 module.exports = async function (_, network, accounts) {
   const dom = await DomToken.deployed();
   await dom.revokeRole(web3.utils.sha3("TRANSFER"), accounts[0]);
   await dom.revokeRole(web3.utils.sha3("TRANSFER_TOGGLER"), accounts[0]);
+  await dom.revokeRole(DEFAULT_ADMIN_ROLE, accounts[0]);
 };
