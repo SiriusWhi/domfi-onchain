ad0b203312c242140c494a3c8d1c9aa6be6da22f
SiriusWhi <dprripontipo480@gmail.com>
2021-08-19 02:10:01 -0500

    Add natspec comments (mostly on public APIs)

--- contracts/test/DummyLPToken.sol
+++ contracts/test/DummyLPToken.sol
@@ -3,10 +3,11 @@ pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/** 
+* @notice Mock LP token for use in testing staking contract
+*/
 contract DummyLPToken is ERC20
 {
-    // this is a mock LP token for use in testing staking contract
-
     constructor(address recipient) ERC20("Dummy LP token", "BTC-FAKEDOM") {
         _mint(recipient, 1000e18);
     }
